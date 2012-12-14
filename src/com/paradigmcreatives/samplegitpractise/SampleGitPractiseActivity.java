3ff0b6fe661b988668bf3f05907cb61599ed2329
sindhura <sindhura.perumalla@gmail.com>
2012-12-14 15:44:26 +0530

    added button anddefined onclick listner to the button

--- src/com/paradigmcreatives/samplegitpractise/SampleGitPractiseActivity.java
+++ src/com/paradigmcreatives/samplegitpractise/SampleGitPractiseActivity.java
@@ -2,12 +2,24 @@ package com.paradigmcreatives.samplegitpractise;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SampleGitPractiseActivity extends Activity {
+	private Button show_Dialog;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		show_Dialog = (Button)findViewById(R.id.showDialog);
+		show_Dialog.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+	
+			}
+		});
 	}
 }

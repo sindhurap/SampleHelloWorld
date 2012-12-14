70940d1ed0b3f27ca68600fe873c6449f236d67d
sindhura <sindhura.perumalla@gmail.com>
2012-12-14 15:48:15 +0530

    Implemented click listner and gave toast upon clickin

--- src/com/paradigmcreatives/samplegitpractise/SampleGitPractiseActivity.java
+++ src/com/paradigmcreatives/samplegitpractise/SampleGitPractiseActivity.java
@@ -5,20 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SampleGitPractiseActivity extends Activity {
 	private Button show_Dialog;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		show_Dialog = (Button)findViewById(R.id.showDialog);
+		show_Dialog = (Button) findViewById(R.id.showDialog);
 		show_Dialog.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-	
+				Toast.makeText(SampleGitPractiseActivity.this,
+						"Show Dialog Button Clicked", Toast.LENGTH_SHORT)
+						.show();
 			}
 		});
 	}
